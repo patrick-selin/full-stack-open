d3d936c3f0290ced6a29b8b46432ae72ef3737d7
Patrick Selin <patrick.selin@gmail.com>
2024-01-30 14:13:40 +0200

    1.8 unicafe step3

--- part1/uniface/src/App.jsx
+++ part1/uniface/src/App.jsx
@@ -1,42 +1,50 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 // #########
 
-// const Button = ( {handleClick, text} ) => {
-//   <button onClick={handleClick} >{text} juu</button>
-// }
-
 const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad, countAll, countAverage, countPositivePercentage }) => {
   return (
-    <button onClick={handleClick}>{text}</button>
+    <div>
+      <h2>statistics</h2>
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {countAll()}</p>
+        <p>average {countAverage()}</p>
+        <p>positive {countPositivePercentage()} %</p>
+      </div>
+    </div>
   );
 };
 
 // #########
 
-
 const App = () => {
-  
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
-    console.log(good)
-  }
+    console.log(good);
+  };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
-  }
+  };
 
   const handleBadClick = () => {
     setBad(bad + 1);
-  }
+  };
 
   const countAll = () => {
     return good + neutral + bad;
-  }
+  };
 
   const countAverage = () => {
     const totalFeedback = countAll(); // 9
@@ -45,7 +53,7 @@ const App = () => {
     }
     const sum = good - bad;
     return sum / totalFeedback;
-  }
+  };
 
   const countPositivePercentage = () => {
     const totalFeedback = countAll();
@@ -58,19 +66,20 @@ const App = () => {
   return (
     <div>
       <h2>Give Feedback</h2>
-      <Button handleClick={handleGoodClick} text="good"/>
-      <Button handleClick={handleNeutralClick} text="neutral"/>
-      <Button handleClick={handleBadClick} text="bad"/>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
 
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all { countAll() }</p>
-      <p>average { countAverage() }</p>
-      <p>positive { countPositivePercentage() } %</p>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        countAll={countAll}
+        countAverage={countAverage}
+        countPositivePercentage={countPositivePercentage}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
