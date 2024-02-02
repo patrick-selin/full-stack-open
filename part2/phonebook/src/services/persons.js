75064d7c4b2041bc430edb6241c26d594f32e360
Patrick Selin <patrick.selin@gmail.com>
2024-02-02 08:55:07 +0200

    2.13 phonebook step8

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -11,13 +11,7 @@ const create = (newObject) => {
   return request.then((response) => response.data);
 };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
-};
-
 export default {
   getAll,
   create,
-  update,
 };
