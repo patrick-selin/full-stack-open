bb4f20a24c59ffdbfd60d21cd6310cc8cddc9a2a
Patrick Selin <patrick.selin@gmail.com>
2024-02-05 12:49:08 +0200

    remove sensitive data

--- part2/data-for-countries/src/services/weather.js
+++ part2/data-for-countries/src/services/weather.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// https://api.openweathermap.org/data/2.5/weather?q=finland&appid=66c59beedea90012a301b8ed1911fb99
-
-// const apiKey = "66c59beedea90012a301b8ed1911fb99";
 const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 console.log(import.meta.env.VITE_OPENWEATHER_API_KEY);
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
