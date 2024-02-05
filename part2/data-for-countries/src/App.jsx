39229d6fbfbec9a44db682cf096c3a02779e9f6f
Patrick Selin <patrick.selin@gmail.com>
2024-02-05 09:28:42 +0200

    2.18 data for countries  step1

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import countryService from "./services/countries";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showPrompt, setShowPrompt] = useState(false);
+
+  useEffect(() => {
+    countryService.getAllCountries().then((data) => {
+      setCountries(data);
+    });
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+    setShowPrompt(false);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  useEffect(() => {
+    // if only one country, get info
+    if (filteredCountries.length === 1) {
+      const countryName = filteredCountries[0].name.common;
+
+      countryService.getCountry(countryName).then((countryInfo) => {
+        console.log("Country info:", countryInfo);
+        // Handle the fetched data, e.g., set it to state
+      });
+    } else if (filteredCountries.length > 10) {
+      setShowPrompt(true);
+    } else {
+      setShowPrompt(false);
+    }
+  }, [filteredCountries]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>find countries</p>
+        <input value={searchQuery} onChange={handleSearch} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        {showPrompt && <p>Too many matches, specify another filter</p>}
+        {!showPrompt && searchQuery.trim() !== "" && (
+          <ul>
+            {filteredCountries.map((country) => (
+              <li key={country.cca2}>{country.name.common}</li>
+            ))}
+          </ul>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
