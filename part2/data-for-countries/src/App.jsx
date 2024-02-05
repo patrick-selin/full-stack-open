20bcc6ef14efac88baa779703832f65fad8bf135
Patrick Selin <patrick.selin@gmail.com>
2024-02-05 12:41:40 +0200

    2.20 data for countries step3

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -68,6 +68,7 @@ function App() {
                 {weather && <WeatherCard weatherData={weather} />}
               </>
             ) : (
+              
               <CountryList
                 countries={filteredCountries}
                 onCountryClick={handleCountryClick}
--- part2/data-for-countries/src/services/weather.js
+++ part2/data-for-countries/src/services/weather.js
@@ -8,7 +8,9 @@ console.log(import.meta.env.VITE_OPENWEATHER_API_KEY);
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 const getCountryWeather = (city) => {
-  const request = axios.get(`${baseUrl}?q=${city}&appid=${apiKey}&units=metric`);
+  const request = axios.get(
+    `${baseUrl}?q=${city}&appid=${apiKey}&units=metric`
+  );
   return request.then((response) => response.data);
 };
 
