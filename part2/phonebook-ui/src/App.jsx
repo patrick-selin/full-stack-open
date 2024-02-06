f685a166710872bb3d970d223122b7a0a5e26b8b
Patrick Selin <patrick.selin@gmail.com>
2024-02-06 09:24:40 +0200

    3.9 phonebook frontend step9

--- part2/phonebook-ui/src/App.jsx
+++ part2/phonebook-ui/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
 
   useEffect(() => {
     personsService.getAll().then((initialNotes) => {
+      console.log(initialNotes);
       setPersons(initialNotes);
     });
   }, []);
--- part2/phonebook-ui/src/services/persons.js
+++ part2/phonebook-ui/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "http://localhost:3001/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
@@ -12,9 +12,9 @@ const createItem = (newObject) => {
 };
 
 const updateItem = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject);
-    return request.then((response) => response.data);
-  };
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
 
 const deleteItem = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`);
