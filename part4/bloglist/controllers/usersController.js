8bef2c0d5e5bba1a18a9178a92bc716fe521f4cc
Patrick Selin <patrick.selin@gmail.com>
2024-02-17 11:07:57 +0200

    4.15 bloglist expansions step3 user controller

--- part4/bloglist/controllers/usersController.js
+++ part4/bloglist/controllers/usersController.js
@@ -1,14 +1,25 @@
 const config = require("../utils/config");
+const bcrypt = require("bcrypt");
 //
 const usersController = require("express").Router();
 const User = require("../models/userModel");
 
 usersController.post("/", async (req, res) => {
-  const newUser = new User(req.body);
-  console.log(`this is juu : ${JSON.stringify(newUser)}`);
+  const { username, name, password } = req.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const newUser = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  console.log(`this is newUser : ${JSON.stringify(newUser)}`);
 
   const savedUser = await newUser.save();
-  res.status(201).json(savedUser.toJSON()); // remove
+  res.status(201).json(savedUser);
 });
 
 module.exports = usersController;
