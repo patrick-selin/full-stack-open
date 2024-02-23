f3a6a529923236d74ff4bd54d9cc252fba514b6e
Patrick Selin <patrick.selin@gmail.com>
2024-02-23 12:11:15 +0200

    5.7 bloglist-ui step6 blogs modal

--- part5/bloglist-ui/src/components/Blog.jsx
+++ part5/bloglist-ui/src/components/Blog.jsx
@@ -1,8 +1,40 @@
-const Blog = ({ blog }) => (
-  // crete new blog post, needs props
-  <div className="blog">
-    {blog.title} {blog.author} {blog.url}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  const handleClick = () => {
+    console.log("object");
+  };
+
+  return (
+    // {showModal &&
+    <div className="blog">
+      <div className="blog-title">
+        <p>{blog.title}</p>
+
+        <button className="left-margin" onClick={toggleShowDetails}>
+          {showDetails ? "hide" : "show"}
+        </button>
+      </div>
+
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>Likes: {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+      
+        </div>
+      )}
+    </div>
+  );
+};
+export default Blog;
+
+// {blog.author} {blog.url}
