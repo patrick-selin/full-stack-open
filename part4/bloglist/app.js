e53d8950e738657ed440a20c0b817c94d05ec24b
Patrick Selin <patrick.selin@gmail.com>
2024-02-27 13:31:17 +0200

    5.17 bloglist-ui step1

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -31,9 +31,14 @@ app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 app.use(middleware.userExtractor);
 //
-app.use('/api/blogs', middleware.userExtractor, blogsRouter);
+app.use("/api/blogs", middleware.userExtractor, blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+//
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testingController");
+  app.use("/api/testing", testingRouter);
+}
 //
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- part5/bloglist-ui/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-ui/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,42 @@
-describe("Note app", function () {
-  it("front page can be opened", function () {
+describe("BlogPost app", function () {
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+
+    const user = {
+      name: "Olen Aaa",
+      username: "aaaa",
+      password: "aaaa",
+    };
+
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:5173");
-    cy.contains("blogs");
   });
+
+  it("5.17 Login form is is shown by defaul", function () {
+    cy.contains("blogs");
+    cy.get('button').contains('Login').should('exist');
+  });
+
+  // describe("Logged in", function () {
+  //   it.skip("5.18", function () {
+  //     // TODO
+  //   });
+  // });
+
+  // it.skip("5.19", function () {
+  //   // TODO
+  // });
+
+  // it.skip("5.20", function () {
+  //   // TODO
+  // });
+  // it.skip("5.21", function () {
+  //   // TODO
+  // });
+  // it.skip("5.22", function () {
+  //   // TODO
+  // });
+  // it.skip("5.23", function () {
+  //   // TODO
+  // });
 });
