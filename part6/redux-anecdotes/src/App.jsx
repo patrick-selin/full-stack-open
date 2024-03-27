8b390e714eef95b8cb65f618ed69545110c6447b
Patrick Selin <patrick.selin@gmail.com>
2024-03-27 16:26:48 +0200

    6.6, Anecdotes step 4 AnecdoteForm

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,21 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { addVote, addAnecdote } from "./reducers/anecdoteReducer";
+import AnecdoteForm from "./components/AnecdoteForm";
 
 const App = () => {
   const anecdotes = useSelector((state) => {
     return state;
   });
   const dispatch = useDispatch();
-
-  const handleAddAnecdote = (id) => {
-    event.preventDefault();
-    // console.log("vote", id);
-
-    const content = event.target.addNewAnecdote.value;
-    event.target.addNewAnecdote.value = "";
-
-    dispatch(addAnecdote(content));
-  };
 
   const handleVote = (id) => {
     // console.log("vote", id);
@@ -37,13 +28,7 @@ const App = () => {
           </div>
         </div>
       ))}
-      <h2>create new</h2>
-      <form onSubmit={handleAddAnecdote}>
-        <div>
-          <input name="addNewAnecdote" />
-        </div>
-        <button>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   );
 };
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,30 @@
+import { useDispatch } from "react-redux";
+import { addAnecdote } from "../reducers/anecdoteReducer";
+
+function AnecdoteForm() {
+  const dispatch = useDispatch();
+
+  const handleAddAnecdote = (id) => {
+    event.preventDefault();
+    console.log("vote", id);
+
+    const content = event.target.addNewAnecdote.value;
+    event.target.addNewAnecdote.value = "";
+
+    dispatch(addAnecdote(content));
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleAddAnecdote}>
+        <div>
+          <input name="addNewAnecdote" />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  );
+}
+
+export default AnecdoteForm;
