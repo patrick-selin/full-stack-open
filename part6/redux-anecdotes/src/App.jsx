c5f3390bb8e74d117ae4239bb7fb96c86731fba4
Patrick Selin <patrick.selin@gmail.com>
2024-03-28 14:47:38 +0200

    6.12, Anecdotes step 10, add Notificattion to App

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,6 +1,7 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
+import Notification from './components/Notification'
 
 
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div id="main-app">
       <h2>Anecdotes</h2>
+      <Notification />
       <Filter />
       <AnecdoteList />
       <AnecdoteForm />
---
+++ part6/redux-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+
+const Notification = () => {
+  // TODO
+  // const notification = useSelector(/* something here */);
+
+  const style = {
+    border: "solid",
+    padding: 10,
+    borderWidth: 1,
+  };
+  return (
+    <div style={style}>
+      TODO notification component
+      {/* {notification} */}
+    </div>
+  );
+};
+
+export default Notification;
