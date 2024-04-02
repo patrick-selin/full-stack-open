166d03195a0540ce8283a85af732e74c45fe9ee6
Patrick Selin <patrick.selin@gmail.com>
2024-04-02 09:48:49 +0300

    6.21, React Query, add new anecdote with RQ

--- part6/query-anecdotes-main/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -1,21 +1,39 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
 const AnecdoteForm = () => {
+  const createAnecdote = (newAnecdote) =>
+    axios
+      .post("http://localhost:3001/anecdotes", newAnecdote)
+      .then((res) => res.data);
+
+  const newAnecdoteMutation = useMutation({ mutationFn: createAnecdote });
+
+  const getRandomId = () => (100000 * Math.random()).toFixed(0);
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    console.log("new anecdote");
+
+    // react query
+    newAnecdoteMutation.mutate({ 
+      content: content,
+      id: getRandomId(),
+      votes: 0
+    });
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
