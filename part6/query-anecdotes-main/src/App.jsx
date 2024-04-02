73190277446f1281ad6db2639edc2f7dac1f410c
Patrick Selin <patrick.selin@gmail.com>
2024-04-02 09:31:52 +0300

    6.21, React Query, retrieve data with RQ, add error handeling

--- part6/query-anecdotes-main/src/App.jsx
+++ part6/query-anecdotes-main/src/App.jsx
@@ -1,18 +1,37 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const App = () => {
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: () =>
+      axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+    retry: 1,
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  }
+
+  if (result.isError) {
+    return (
+      <>
+        <p>Anecdote service not available due to problems in server</p>
+        <p>Eror: {result.error.message}</p>
+      </>
+    );
+  }
+
+  const anecdotes = result.data;
+
+  //
+
   const handleVote = (anecdote) => {
     console.log("vote");
   };
-
-  const anecdotes = [
-    {
-      content: "If it hurts, do it more often",
-      id: "47145",
-      votes: 0,
-    },
-  ];
 
   return (
     <div>
