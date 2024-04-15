3eae10ef5370f25b54b09cbfae1058bedc8c5ad3
Patrick Selin <patrick.selin@gmail.com>
2024-04-15 10:30:47 +0300

    7.14 Views, User view, done

--- part7/blog-app-sql/frontend/src/components/Menu.jsx
+++ part7/blog-app-sql/frontend/src/components/Menu.jsx
@@ -5,16 +5,22 @@ import { Link } from "react-router-dom";
 const Menu = () => {
   const padding = {
     paddingRight: 5,
+    marginBottom: 15,
+  };
+
+  const nav = {
+    marginBottom: "3rem",
   };
 
   return (
-    <div>
+    <div style={nav}>
       <Link to="/" style={padding}>
         blogs
       </Link>
       <Link to="/users" style={padding}>
         users
       </Link>
+      {/* user log in / singed user */}
     </div>
   );
 };
--- part7/blog-app-sql/frontend/src/components/UsersList.jsx
+++ part7/blog-app-sql/frontend/src/components/UsersList.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
-// redux 
+// redux
 
-const UsersList = ( users ) => {
-  return <div>List of users </div>;
+const UsersList = ({ users }) => {
+  return (
+    <div>
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Blogs Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UsersList;
