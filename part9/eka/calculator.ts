8a16334589ece2db77c7e432c33b5865acff4219
Patrick Selin <patrick.selin@gmail.com>
2024-04-25 09:29:34 +0300

    multiplier advance

---
+++ part9/eka/calculator.ts
@@ -0,0 +1,34 @@
+type Operation = "multiply" | "add" | "divide";
+
+const calculator = (a: number, b: number, op: Operation): number => {
+  switch (op) {
+    case "multiply":
+      return a * b;
+    case "add":
+      return a + b;
+    case "divide":
+      if (b === 0) {
+        throw new Error("Can't divide by 0");
+      }
+      return a / b;
+    default:
+      throw new Error("Operation is not multiply, add or divide!");
+  }
+}; 
+
+try {
+  const tulos = calculator(1, 2, "add");
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong ";
+
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
+
+// const tulos = calculator(1, 2, "add");
+// const tulos2 = calculator(8, 0, "divide");
+
+// console.log(tulos);
+// console.log(tulos2);
--- part9/eka/multiplier.ts
+++ part9/eka/multiplier.ts
@@ -1,29 +1,48 @@
-type Operation = "multiply" | "add" | "divide";
+// make interface
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
+console.log(process.argv);
 
-const calculator = (a: number, b: number, op: Operation): number => {
-  switch (op) {
-    case "multiply":
-      return a * b;
-    case "add":
-      return a + b;
-    case "divide":
-      if (b === 0) {
-        throw new Error("Can't divide by 0");
-      }
-      return a / b;
-    default:
-      throw new Error("Operation is not multiply, add or divide!");
+process.argv.forEach((arg, index) => {
+  console.log(`Argument ${index}: ${arg}`);
+});
+
+// parse the arguments
+const parseArguments = (args: string[]): MultiplyValues => {
+  if (args.length < 4) {
+    throw new Error("Not enough arguments");
+  }
+  if (args.length > 4) {
+    throw new Error("Too many arguments");
+  }
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
   }
 };
 
+// the program
 const multiplicator = (a: number, b: number, printText: string): void => {
   console.log(printText, a * b);
 };
 
 multiplicator(2, 4, "Multiplied numbers 2 and 4, the result is:");
 
+// try-catch
 try {
-  const tulos = calculator(1, 2, "add");
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(
+    value1,
+    value2,
+    `Multiplied ${value1} and ${value2}, the result is:`
+  );
 } catch (error: unknown) {
   let errorMessage = "Something went wrong ";
 
@@ -32,9 +51,3 @@ try {
   }
   console.log(errorMessage);
 }
-
-// const tulos = calculator(1, 2, "add");
-// const tulos2 = calculator(8, 0, "divide");
-
-// console.log(tulos);
-// console.log(tulos2);
