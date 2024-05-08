e0beca9edc4f91f817fe792e32869b7026634ebe
Patrick Selin <patrick.selin@gmail.com>
2024-05-08 17:49:29 +0300

    9.8 backend init

---
+++ part9/patientor-backend/index.ts
@@ -0,0 +1,47 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+
+//
+// app.post("/exercises", (req, res) => {
+//   const { daily_exercises, target } = req.body;
+
+//   if (!daily_exercises || !target) {
+//     return res.status(400).json({ error: "parameters missing" });
+//   }
+
+//   if (
+//     !Array.isArray(daily_exercises) ||
+//     daily_exercises.some((value) => typeof value !== "number") ||
+//     typeof target !== "number"
+//   ) {
+//     return res.status(400).json({ error: "malformatted parameters" });
+//   }
+
+//   try {
+//     const exerciseStats = calculateExercises(daily_exercises, target);
+//     // console.log(`THIS :: :: ${JSON.stringify(exerciseStats)}`);
+
+//     return res.json(exerciseStats).status(200);
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       res.status(400).send({ error: error.message });
+//     }
+
+//     return res.status(400).send({ error: "something went wrong" });
+//   }
+// });
+//
+
+const PORT = 3005;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
