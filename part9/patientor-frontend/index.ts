0d07fc7a3aa4806aeb7c71d7ad35c489ee2ea062
Patrick Selin <patrick.selin@gmail.com>
2024-05-08 17:36:39 +0300

    9.8 Patientor backend init b

--- part9/patientor-frontend/index.ts
+++ part9/patientor-frontend/index.ts
@@ -5,55 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.get("/hello", (_req, res) => {
-  res.send("Hello Full Stack!");
+  return res.send("Hello Full Stack!");
 });
 
-app.get("/bmi", (req, res) => {
-  const height = Number(req.query.height);
-  const weight = Number(req.query.weight);
-
-  if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
-    return res.status(400).json({ error: "malformatted parameters" });
-  }
-
-  const bmiResult = calculateBmi(height, weight);
-
-  return res.json({
-    weight,
-    height,
-    bmi: bmiResult,
-  });
-});
-
-//
-app.post("/exercises", (req, res) => {
-  const { daily_exercises, target } = req.body;
-
-  if (!daily_exercises || !target) {
-    return res.status(400).json({ error: "parameters missing" });
-  }
-
-  if (
-    !Array.isArray(daily_exercises) ||
-    daily_exercises.some((value) => typeof value !== "number") ||
-    typeof target !== "number"
-  ) {
-    return res.status(400).json({ error: "malformatted parameters" });
-  }
-
-  try {
-    const exerciseStats = calculateExercises(daily_exercises, target);
-    // console.log(`THIS :: :: ${JSON.stringify(exerciseStats)}`);
-
-    return res.json(exerciseStats).status(200);
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(400).send({ error: error.message });
-    }
-
-    return res.status(400).send({ error: "something went wrong" });
-  }
-});
 //
 
 const PORT = 3005;
