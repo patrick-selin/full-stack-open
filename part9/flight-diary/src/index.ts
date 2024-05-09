3bdca6ff3aca9540290ad239083aa1749ea73360
Patrick Selin <patrick.selin@gmail.com>
2024-05-09 10:20:18 +0300

    flight app add json and diaaries route

--- part9/flight-diary/src/index.ts
+++ part9/flight-diary/src/index.ts
@@ -1,6 +1,7 @@
 // flight-diary app
 
 import express from 'express';
+import diaryRouter from './routes/diaries';
 const app = express();
 app.use(express.json());
 
@@ -10,6 +11,8 @@ app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
