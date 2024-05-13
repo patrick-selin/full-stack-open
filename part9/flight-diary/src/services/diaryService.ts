1e7db6ae3be99a1d66cb790115721b011d256925
Patrick Selin <patrick.selin@gmail.com>
2024-05-13 08:48:02 +0300

    nonSensitiveDiaryEntry types and service added

--- part9/flight-diary/src/services/diaryService.ts
+++ part9/flight-diary/src/services/diaryService.ts
@@ -1,8 +1,17 @@
 import diaries from "../../data/entries";
-import { DiaryEntry } from "../types";
+import { DiaryEntry, NonSensitiveDiaryEntry } from "../types";
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
 };
 
 const addDiary = () => {
@@ -12,4 +21,5 @@ const addDiary = () => {
 export default {
   getEntries,
   addDiary,
+  getNonSensitiveEntries
 };
