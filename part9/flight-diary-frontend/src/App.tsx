d68429e1300b74eeacc24eddc73fe63f44cf3907
Patrick Selin <patrick.selin@gmail.com>
2024-05-14 14:21:41 +0300

    9.16 refactor fetches to service/diaries

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,28 +1,88 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { getAllDiaries, createDiary } from "./services/diaries";
 //
-import { DiaryEntry } from './types';
-
+import { DiaryEntry, NewDiaryEntry, Weather, Visibility } from "./types";
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
-  // const [newDiary, setNewDiary] = useState("");
+  const [newDiary, setNewDiary] = useState<NewDiaryEntry>({} as NewDiaryEntry);
   //
   useEffect(() => {
-    axios
-      .get<DiaryEntry[]>("http://localhost:3003/api/diaries")
-      .then((response) => {
-        setDiaries(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching diaries:", error);
-      });
+    getAllDiaries().then((data) => {
+      setDiaries(data);
+    });
   }, []);
   //
 
+  const createNewDiary = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    console.log(newDiary);
+
+    axios
+      .post<DiaryEntry>("http://localhost:3003/api/diaries", newDiary)
+      .then((response) => {
+        setDiaries(diaries.concat(response.data));
+      });
+    setNewDiary({} as NewDiaryEntry);
+  };
+
   return (
     <>
-      <h1>Diary Entries</h1>
+      <h2>Diary Entries</h2>
+      <form onSubmit={createNewDiary}>
+        <div>
+          date
+          <input
+            value={newDiary.date || ""}
+            onChange={(event) =>
+              setNewDiary((prevState) => ({
+                ...prevState,
+                date: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <div>
+          weather
+          <input
+            value={newDiary.weather || ""}
+            onChange={(event) =>
+              setNewDiary((prevState) => ({
+                ...prevState,
+                weather: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <div>
+          visibility
+          <input
+            value={newDiary.visibility || ""}
+            onChange={(event) =>
+              setNewDiary((prevState) => ({
+                ...prevState,
+                visibility: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <div>
+          comment
+          <input
+            value={newDiary.comment || ""}
+            onChange={(event) =>
+              setNewDiary((prevState) => ({
+                ...prevState,
+                comment: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+
+      <h2>Diary Entries</h2>
       <ul>
         {diaries.map((diary, index) => (
           <li key={index}>
--- part9/flight-diary-frontend/src/types.ts
+++ part9/flight-diary-frontend/src/types.ts
@@ -3,6 +3,24 @@
 export interface DiaryEntry {
   id: number;
   date: string;
-  weather: string;
-  visibility: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment?: string;
 }
+
+export enum Weather {
+    Sunny = "sunny",
+    Rainy = "rainy",
+    Cloudy = "cloudy",
+    Stormy = "stormy",
+    Windy = "windy",
+  }
+  
+  export enum Visibility {
+    Great = 'great',
+    Good = 'good',
+    Ok = 'ok',
+    Poor = 'poor',
+  }
+  
+export type NewDiaryEntry = Omit<DiaryEntry, "id">;
